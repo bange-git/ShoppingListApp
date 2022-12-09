5b4dd58851e46fcea4b34d919e704166fd5b85ab
bange-git <sebastienbange@gmail.com>
2022-12-09 05:09:48 +0100

    added components and implemented delete logic

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ import {
   FlatList,
   TextInput,
   StyleSheet,
-  Pressable,
 } from 'react-native';
-import ListItem from './components/ListItem';
-
+import ListItem from './src/components/ListItem';
+//import {v4 as uuid} from 'uuidv4';
+import 'react-native-get-random-values';
+//import {v4 as uuid} from 'uuid';
+//import {v4} from 'uuid';
 const App = () => {
   const [inputText, setInputText] = useState('');
   const [itemList, setItemList] = useState([
@@ -50,15 +52,17 @@ const App = () => {
         <Button title="Add Item" onPress={addItemHandler} />
       </View>
       <View style={styles.listStyles}>
-        {itemList.map(item => (
-          <ListItem
-            text={item.text}
-            id={item.id}
-            onRemove={onDelete}
-            key={item.id}
-          />
-        ))}
-        <FlatList />
+        <FlatList
+          data={itemList}
+          renderItem={({item}) => (
+            <ListItem
+              text={item.text}
+              id={item.id}
+              onRemove={onDelete}
+              key={item.id}
+            />
+          )}
+        />
       </View>
     </View>
   );
@@ -109,120 +113,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
-
-// import React from 'react';
-// import type {Node} from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
-//  * LTI update could not be added via codemod */
-// const Section = ({children, title}): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
-// const App: () => Node = () => {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.js</Text> I LOVE BANGE
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
 import {View, Pressable, Text, StyleSheet} from 'react-native';
+//import Icon from 'react-native-vector-icons/Ionicons';
+
 const ListItem = ({text, id, onRemove}) => {
   const deleteHandler = () => {
     onRemove(id);
   };
   return (
     <Pressable onPress={deleteHandler} key={id}>
-      <View>
-        <Text style={styles.listings}>{text}</Text>
+      <View style={styles.listItems}>
+        <Text style={styles.listText}>{text}</Text>
+        {/* <Icon name="remove" color="firebrick" size={20} /> */}
       </View>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  listings: {
+  listText: {
+    fontSize: 18,
+  },
+  listItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     backgroundColor: 'white',
     borderWidth: 1,
     borderColor: '#e4e4e4',
